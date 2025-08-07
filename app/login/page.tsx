c6a46b7ef414b9d408723ec59c15bb30724071e4
abellmanuell <mannydev02@gmail.com>
2025-08-07 15:28:39 +0100

    Enhance the login design

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -28,10 +28,11 @@ export default function Page() {
   return (
     <section className="grid md:grid-cols-[600px_1fr]">
       <div className="p-20 min-h-dvh">
-        <div className="my-10">
-          <h1 className="text-2xl font-bold">Login to BluuPay</h1>
-          <p className="text-sm text-gray-500">
-            Everything You Need To Sell More.
+        <div className="my-10 space-y-2">
+          <h1 className="text-4xl font-bold mb-4">Login to BluuPay</h1>
+          <p className="text-sm text-gray-500 font-poppins">
+            BluuPay helps you sell, manage inventory, and track performance—all
+            from one simple, cloud-based platform that works on any device.
           </p>
         </div>
 
@@ -41,6 +42,8 @@ export default function Page() {
               placeholder="Email address"
               value={email}
               onChange={(e) => setEmail(e.currentTarget.value)}
+              size="lg"
+              radius="md"
             />
           </div>
           <div>
@@ -49,24 +52,22 @@ export default function Page() {
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.currentTarget.value)}
+              size="lg"
+              radius="md"
             />
           </div>
-
-          <div>
-            <Link
-              href="/forgot_password"
-              className="text-sm text-blue-600 text-right block"
-            >
-              Forgot Password?
+          <div className="text-right">
+            <Link href="" className="text-blue-600 text-sm text-right">
+              Forget Password
             </Link>
           </div>
 
-          <PrimaryButton onClick={handleSubmit}>Log in</PrimaryButton>
+          <PrimaryButton onClick={handleSubmit}>Get Started</PrimaryButton>
 
           <p className="text-sm">
             Don&apos;t have an account?{" "}
-            <Link href="/signup" className="text-blue-600 font-bold">
-              Sign up
+            <Link className="text-blue-600 font-bold" href="/register">
+              Register
             </Link>
           </p>
         </form>
