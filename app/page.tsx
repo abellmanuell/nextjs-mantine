61a8e0825eb3c9f5587e123c6e747e37eda5477d
abellmanuell <mannydev02@gmail.com>
2025-08-07 11:02:53 +0100

    List nav links

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 export default function Demo() {
   return (
     <>
-      <h1>Dashboard</h1>
+      <ul className="p-20">
+        <li>
+          <Link href="/dashboard" className="underline text-sky-500">
+            Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link href="/login" className="underline text-sky-500">
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link href="/signup" className="underline text-sky-500">
+            Sign up
+          </Link>
+        </li>
+        <li>
+          <Link href="/reset_password" className="underline text-sky-500">
+            Reset password
+          </Link>
+        </li>
+        <li></li>
+      </ul>
     </>
   );
 }
