1b2ada4e079b3f1f73fbc55b7081a6478bb99ebe
abellmanuell <mannydev02@gmail.com>
2025-08-07 11:17:35 +0100

    Redesign forget and reset password

--- app/forgot_password/page.tsx
+++ app/forgot_password/page.tsx
@@ -5,23 +5,28 @@ import PrimaryButton from "../ui/PrimaryButton";
 
 export default function ForgotPassword() {
   return (
-    <div className="p-20 space-y-4 w-2/5 m-auto">
-      <div className="my-10">
-        <h1 className="text-2xl font-bold">Forgot Password?</h1>
+    <section className="grid md:grid-cols-[600px_1fr]">
+      <div className="p-20 min-h-dvh">
+        <div className="mt-10 mb-5">
+          <h1 className="text-2xl font-bold">Forgot Password?</h1>
+        </div>
+
+        <form className="space-y-4">
+          <div>
+            <Input placeholder="Email address" name="email" />
+          </div>
+
+          <PrimaryButton>Send</PrimaryButton>
+          <p className="text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="/signup" className="text-sky-500 font-bold">
+              Sign up
+            </Link>
+          </p>
+        </form>
       </div>
 
-      <form action={""}>
-        <Input placeholder="Email address" name="email" />
-      </form>
-
-      <PrimaryButton>Send</PrimaryButton>
-
-      <p className="text-sm">
-        Don&apos;t have an account?{" "}
-        <Link href="/signup" className="text-sky-500 font-bold">
-          Sign up
-        </Link>
-      </p>
-    </div>
+      <div className="min-h-dvh bg-sky-100 hidden md:block"></div>
+    </section>
   );
 }
--- app/reset_password/page.tsx
+++ app/reset_password/page.tsx
@@ -4,22 +4,26 @@ import PrimaryButton from "../ui/PrimaryButton";
 
 export default function ForgotPassword() {
   return (
-    <div className="p-20 space-y-4 w-2/5 m-auto">
-      <div className="my-4">
-        <h1 className="text-2xl font-bold">Reset Password</h1>
+    <section className="grid md:grid-cols-[600px_1fr]">
+      <div className="p-20 min-h-dvh">
+        <div className="my-4">
+          <h1 className="text-2xl font-bold">Reset Password</h1>
+        </div>
+
+        <form action={""} className="space-y-4">
+          <div>
+            <Input placeholder="New password" name="new_password" />
+          </div>
+
+          <div>
+            <Input placeholder="Confirm Password" name="confirm_password" />
+          </div>
+
+          <PrimaryButton>Reset password</PrimaryButton>
+        </form>
       </div>
 
-      <form action={""} className="space-y-4">
-        <div>
-          <Input placeholder="New password" name="new_password" />
-        </div>
-
-        <div>
-          <Input placeholder="Confirm Password" name="confirm_password" />
-        </div>
-      </form>
-
-      <PrimaryButton>Reset password</PrimaryButton>
-    </div>
+      <div className="min-h-dvh bg-sky-100 hidden md:block"></div>
+    </section>
   );
 }
